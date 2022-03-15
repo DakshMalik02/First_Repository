0cac7085dd08235c4613993c9195ccf9d193f070
DakshMalik02 <daksh0399.be21@chitkara.edu.in>
2022-03-15 13:28:34 +0530

    Sample text

---
+++ First.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a,b;
+	cout<<"Enter two numbers: "<<endl;
+	cin>>a;
+	cin>>b;
+	int sum = a+b;
+	cout<<"Sum of the numbers is: "<<sum;
+}
